be049e88442a509bf0e437bf2839743557901e44
Ayush Kumar <ayushkr0104@hotmail.com>
2025-01-24 00:57:46 +0530

    nav

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
     <Disclosure as="nav" className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-white' : 'bg-transparent'}`}>
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className=" px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16">
               {/* Logo */}
               <div className="flex-shrink-0">
